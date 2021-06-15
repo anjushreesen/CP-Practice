e5d116c32637e52bc294948b8b64136d7c357c09
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-15 22:32:38 +0530

    array rotation using mod technique

---
+++ GFG/RotateArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //code
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, d;
+        cin >> n >> d;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[(i + n - d) % n];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
