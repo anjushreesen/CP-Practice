f612d57f8255a704942d4b2118771f2c03937751
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-18 18:07:19 +0530

    CountTheZeros.cpp

---
+++ GFG/CountTheZeros.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+// } Driver Code Ends
+//User function Template for C
+
+int countZeroes(int arr[], int n)
+{
+    // code here
+    int count = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (arr[i] == 0)
+        {
+            count++;
+        }
+        else
+        {
+            return count;
+        }
+    }
+
+    return count;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+            ;
+        }
+
+        int ans = countZeroes(arr, n);
+        printf("%d\n", ans);
+    }
+    return 0;
+}
+// } Driver Code Ends
