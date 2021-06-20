2223db493d73a8d9507b85dd053e86865db174f5
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-20 12:39:44 +0530

    segregating 0's and 1's

---
+++ GFG/Segregation.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+public:
+    void segregate0and1(int arr[], int n)
+    {
+        // code here
+        int i = 0, j = n - 1;
+        while (i < j)
+        {
+            while (arr[i] != 1 && i < j)
+            {
+                i++;
+            }
+            while (arr[j] != 0 && j > i)
+            {
+                j--;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.segregate0and1(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+} // } Driver Code Ends
