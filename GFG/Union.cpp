5d470a42ec03f366dfd6f9b8df18cc3b5aa42ecd
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-16 11:48:43 +0530

    Union of two arrays

---
+++ GFG/Union.cpp
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template in C++
+
+class Solution
+{
+public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)
+    {
+        //code here
+        int i{0}, count{0};
+        int arr[m + n];
+        for (i = 0; i < n; i++)
+        {
+            arr[i] = a[i];
+        }
+        for (int j = i, k = 0; k < m; k++, j++)
+        {
+            arr[j] = b[k];
+        }
+
+        sort(arr, arr + m + n);
+        for (int i = 0; i < m + n; i++)
+        {
+            if (arr[i] != arr[i + 1])
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n, m;
+        cin >> n >> m;
+        int a[n], b[m];
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        for (int i = 0; i < m; i++)
+            cin >> b[i];
+        Solution ob;
+        cout << ob.doUnion(a, n, b, m) << endl;
+    }
+
+    return 0;
+} // } Driver Code Ends
